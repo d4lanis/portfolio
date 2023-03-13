056fa58214d06665e0faec52e5dbe7b55e3b6947
Daniel Alanis <daniel.alanis.hdz@gmail.com>
2023-03-13 05:02:22 +0000

    social media added linkein github, one is missing

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,12 +1,15 @@
+import GithubIcon from "../../assets/github.svg"
+import LinkedInIcon from "../../assets/linkedin.svg"
+
 function NavBar () {
 
 
     return (
         <nav style={{position: "fixed", top: "0%", left: "0%", width: "100%", paddingTop: "1%"}}>
-            <ul style={{display: "flex", justifyContent: "space-evenly"}}>
+            <ul style={{display: "flex", justifyContent: "space-evenly", alignItems: "center"}}>
                 <li>LOGO</li>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
+                <li><a href="#"><img src={GithubIcon} style={{filter: "invert(100%)", width: "50px"}}/></a></li>
+                <li><a href="#"><img src={LinkedInIcon} style={{filter: "invert(100%)", width: "80px"}}/></a></li>
                 <li><a href="#">Contact</a></li>
              </ul>
         </nav>
