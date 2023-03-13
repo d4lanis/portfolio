a9eb3d39afb63f4afd734de88cf34a97744f645c
Daniel Alanis <daniel.alanis.hdz@gmail.com>
2023-03-13 16:55:40 +0000

    add links to github, credly and linkedin

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,6 @@
-import GithubIcon from "../../assets/github.svg"
-import LinkedInIcon from "../../assets/linkedin.svg"
+import GithubIcon from "../../assets/github.svg";
+import LinkedInIcon from "../../assets/linkedin.svg";
+import CredlyIcon from "../../assets/credly.svg";
 
 function NavBar () {
 
@@ -8,9 +9,9 @@ function NavBar () {
         <nav style={{position: "fixed", top: "0%", left: "0%", width: "100%", paddingTop: "1%"}}>
             <ul style={{display: "flex", justifyContent: "space-evenly", alignItems: "center"}}>
                 <li>LOGO</li>
-                <li><a href="#"><img src={GithubIcon} style={{filter: "invert(100%)", width: "50px"}}/></a></li>
-                <li><a href="#"><img src={LinkedInIcon} style={{filter: "invert(100%)", width: "80px"}}/></a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a target={"blank"} href="https://www.github.com/d4lanis"><img src={GithubIcon} style={{filter: "invert(100%)", width: "50px"}}/></a></li>
+                <li><a target={"blank"} href="https://www.linkedin.com/in/daniel-alanis-569628194/"><img src={LinkedInIcon} style={{filter: "invert(100%)", width: "70px"}}/></a></li>
+                <li><a target={"blank"} href="https://www.credly.com/users/daniel-alanis-hernandez"><img src={CredlyIcon} style={{width:"50px"}}/></a></li>
              </ul>
         </nav>
     );
