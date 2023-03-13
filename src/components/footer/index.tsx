41e4b44a7de1cbf288b88396be7187cd2a0346f7
Daniel Alanis <daniel.alanis.hdz@gmail.com>
2023-03-13 16:56:03 +0000

    add copyright logo on footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,7 @@ function Footer () {
     return (
         <footer style={{position: "fixed", bottom: "0%", left: "0%", width: "100%", paddingBottom: "1%"}}>
             <ul style={{display: "flex", justifyContent: "center"}}>
-                <li>{new Date().getFullYear()} Daniel Alanis.</li>
+                <li>{new Date().getFullYear()} &#169; - Daniel Alanis.</li>
             </ul>
         </footer>
     );
