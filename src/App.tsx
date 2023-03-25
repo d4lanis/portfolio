73df6b43496666cc67ea3b41a3bbbea4be4f3c0d
Daniel Alanis <daniel.alanis.hdz@gmail.com>
2023-03-25 04:53:53 +0000

    add work break to paragraphs

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,28 @@ function App() {
 
   return (
     <div>
-      <NavBar/>
-      <div className="main-section">
-        <section>
-        <h1>Welcome.</h1>
-        <p>
-            My name is Daniel Alanis. I'm Web Developer based in Satillo, Coahuila. MX I have developed many types of projects
-        </p>
-        <p>
-            I'm passionate about technology, videogames and music.
-        </p>
+      <NavBar />
+      <div className="main-section" style={{display: "flex", overflow: "auto"}}>
+        <section style={{wordWrap: "break-word", width: "30%"}}>
+          <h1>Welcome.</h1>
+          <p>
+            My name is Daniel Alanis and I am a web developer with 2 years of experience. I specialize in full-stack development, but I have a particular passion for backend programming.
+          </p>
+          <p>
+            Throughout my career, I have worked on a variety of projects that have allowed me to hone my skills and expand my knowledge. From developing custom APIs to building scalable microservices, I have experience working on projects of all sizes and complexities.
+          </p>
+          <p>
+            My technical skills include proficiency in multiple programming languages such as Typescript, Javascript, as well as expertise in popular web development frameworks and technologies such as React, Redux. Additionally, I am well-versed in cloud technologies such as AWS, which allows me to develop scalable and efficient solutions.
+          </p>
         </section>
-        <section>
+        <section style={{wordWrap: "break-word", width: "20%"}}>
           <h1>Projects</h1>
           <ul>
             <li>Stellar Menus</li>
-            <li>Scaling up</li>
+            <li>Scaling Pp</li>
             <li>Goverment-platform</li>
             <li>To-Do App</li>
+            <li>N-TECH IT Support Page</li>
           </ul>
         </section>
       </div>
